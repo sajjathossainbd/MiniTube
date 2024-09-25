767bd19096c9127dcd0fc8662ad642e788bf22d1
sajjathossainbd <sajjadsadiq2020@gmail.com>
2024-09-25 13:28:24 +0600

    add a new featured: pagination and footer section create

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,20 @@
+function Footer() {
+  return (
+    <section className="pt-6">
+      <div className="max-w-7xl mx-auto px-5 py-6 lg:px-0 flex justify-between gap-2 border-t text-sm text-slate-400">
+        <div>Copyright 2024 - MiniTube</div>
+        <div>
+          <a
+            href="https://www.youtube.com/@SajjadSadiq"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Sajjat Hossain
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
+import Footer from "../components/footer/footer";
 import Navbar from "../components/navbar/Navbar";
 import Tags from "../components/tags/Tags";
+import Pagination from "../components/ui/Pagination";
 import Videos from "../components/videos/Videos";
 
 function Home() {
@@ -8,6 +10,8 @@ function Home() {
       <Navbar />
       <Tags />
       <Videos />
+      <Pagination />
+      <Footer />
     </div>
   );
 }
