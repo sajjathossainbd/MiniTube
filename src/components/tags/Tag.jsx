0fbac9d5b7af725ebcd5aeeb33d8fa2a63bcb41b
sajjathossainbd <sajjadsadiq2020@gmail.com>
2024-09-26 17:32:37 +0600

    tags data load and ui show

--- src/components/tags/Tag.jsx
+++ src/components/tags/Tag.jsx
@@ -1,8 +1,9 @@
-function Tag() {
+/* eslint-disable react/prop-types */
+function Tag({ tag }) {
   return (
     <>
       <div className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer">
-        react
+        {tag.title}
       </div>
 
       {/* <div className="bg-blue-600 text-white px-4 py-1 rounded-full cursor-pointer">
--- src/components/tags/Tags.jsx
+++ src/components/tags/Tags.jsx
@@ -1,13 +1,24 @@
+import { useDispatch, useSelector } from "react-redux";
 import Tag from "./Tag";
+import { useEffect } from "react";
+import { fetchTags } from "../../features/tags/tagsSlice";
 
 function Tags() {
-  return (
+  const dispatch = useDispatch();
+  const { tags } = useSelector((state) => state.tags);
+  useEffect(() => {
+    dispatch(fetchTags());
+  }, [dispatch]);
+
+  return tags?.length > 0 ? (
     <section>
       <div className="max-w-7xl mx-auto px-5 py-6 lg:px-0 flex gap-2 border-b overflow-y-auto">
-        <Tag />
+        {tags.map((tag) => (
+          <Tag key={tag.id} tag={tag} />
+        ))}
       </div>
     </section>
-  );
+  ) : null;
 }
 
 export default Tags;
